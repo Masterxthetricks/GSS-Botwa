5fc36bb52ace2ed9a9f6d6cc6b3c069f162e7590
Masterxthetricks <165632298+Masterxthetricks@users.noreply.github.com>
2025-12-30 22:36:38 -0500

    Fix variable name for server port

--- index.js
+++ index.js
@@ -128,6 +128,6 @@ async function startHisoka() {
 
 startHisoka(); 
 app.get('/', (req, res) => res.send('Bot Status: Online'));
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is listening on port ${port}`);
 });
