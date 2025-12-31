ec73571c9c3f02109d468ccfdcb6ff65248d685e
Masterxthetricks <165632298+Masterxthetricks@users.noreply.github.com>
2025-12-30 21:36:21 -0500

    Start server and log running port

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ if (!fs.existsSync('./antilink.json')) {
 const express = require('express') 
 const app = express() 
 const port = process.env.PORT || 8000
-
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${port}`);
+});
 const { 
    default: goutamConnect, 
    useMultiFileAuthState, 
